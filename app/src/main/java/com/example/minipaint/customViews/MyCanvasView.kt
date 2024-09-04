d38f6833811d943e0dc977d90ecddece4da58d4f
Amanda Soares <amanda.soares@ifood.com.br>
2024-09-04 11:24:33 -0300

    Exercise: add frame

--- app/src/main/java/com/example/minipaint/customViews/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/customViews/MyCanvasView.kt
@@ -5,19 +5,28 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 import com.example.minipaint.R
 
 private const val STROKE_WIDTH = 12f
 
 class MyCanvasView(context: Context) : View(context) {
+
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
+    private lateinit var frame: Rect
 
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
     private val backgroundColor = ResourcesCompat
         .getColor(resources, R.color.colorBackground, null)
@@ -46,6 +55,9 @@ class MyCanvasView(context: Context) : View(context) {
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -65,10 +77,30 @@ class MyCanvasView(context: Context) : View(context) {
         return true
     }
 
-    private fun touchStart() {}
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
 
-    private fun touchMove() {}
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
 
-    private fun touchUp() {}
+    private fun touchUp() {
+        path.reset()
+    }
 
 }
